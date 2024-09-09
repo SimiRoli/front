12d4c727f3954f95e95dde7c63491e5cabf35948
Simon Roland <roland.simon@katonaj-mkszig.hu>
2024-09-09 08:12:18 +0200

    asd

---
+++ elso/Igen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Igen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ananasz leves = new ananasz();
+            leves.korte();
+        }
+    }
+}
---
+++ elso/Igen/ananasz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igen
+{
+    internal class ananasz
+    {
+        public ananasz()
+        {
+        
+        }
+        public void korte()
+        {
+            Console.WriteLine("Adj meg egy számot!");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (szam % 2 == 0)
+            {
+                Console.WriteLine("A szám páros");
+            }
+            if (szam % 2 == 1)
+            {
+                Console.WriteLine("A szám páratlan");
+            }
+            Console.ReadKey();
+        }
+    }
+}
